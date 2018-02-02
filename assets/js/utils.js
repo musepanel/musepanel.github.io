aeac26ca92cfae2b962bfffaf9ac2fe74a63c212
Sridatt Bhamidipati <sridattbhamidipati@Sridatts-MacBook-Pro.local>
2018-02-01 22:47:53 -0800

    move firebase back and minify

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,17 +1,1 @@
-var utils = {};
-
-utils.shorten = function(str, maxLength){
-	if (str.length <= maxLength)
-		return str;
-	
-	var resultStr = str.substr(0, maxLength);
-	return (resultStr[resultStr.length - 1] == " " ? resultStr.slice(0, -1) : resultStr) + "...";
-}
-
-utils.properCaps = function(song, inp){
-	return song.substr(song.toLowerCase().indexOf(inp), inp.length);
-}
-
-utils.hashSong = function(song, artist){
-	return (song + "-" + artist).split(' ').join('').toLowerCase();
-}
+var utils={};utils.shorten=function(str,maxLength){if(str.length<=maxLength)return str;var resultStr=str.substr(0,maxLength);return(resultStr[resultStr.length-1]==" "?resultStr.slice(0,-1):resultStr)+"..."};utils.properCaps=function(song,inp){return song.substr(song.toLowerCase().indexOf(inp),inp.length)};utils.hashSong=function(song,artist){return(song+"-"+artist).split(" ").join("").toLowerCase()};
